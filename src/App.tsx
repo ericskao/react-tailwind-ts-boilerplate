e1502e7238f45e833f0591c611961970f62c0a2c
Eric K <ericskao@gmail.com>
2023-04-08 14:42:23 -0700

    boilerplate experience, add .env to gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
+import logo from './logo.svg';
 import './App.scss';
 
 function App() {
-  useEffect(() => {
-    console.log('use Effect');
-  }, []);
-
-  return <div>start</div>;
+  return (
+    <div className="App">
+      <div className="text-blue-500">
+        Test tailwind <div className="App__here">here</div>
+      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <button className="bg-slate-50">Click here to get weather</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
